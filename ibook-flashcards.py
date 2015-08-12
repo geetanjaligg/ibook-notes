a18e3f5031c4ba897d3c6d5a3512ca059a8d446e
geetanjaligg <anjaligg@gmail.com>
2015-08-12 19:33:49 +0530

    get all highlights n books. Yet to implement word meaning

---
+++ ibook-flashcards.py
@@ -0,0 +1,39 @@
+import os
+import sqlite3
+import random
+
+bookDB = '/Users/gggg/Library/Containers/com.apple.iBooksX/Data/Documents/BKLibrary/'
+notesDB = '/Users/gggg/Library/Containers/com.apple.iBooksX/Data/Documents/AEAnnotation/'
+
+connectB = sqlite3.connect(bookDB + [f for f in os.listdir(bookDB) if f.endswith('.sqlite')][0])
+c = connectB.cursor()
+c.execute('SELECT ZASSETID, ZTITLE AS Title, ZAUTHOR AS Author FROM ZBKLIBRARYASSET WHERE ZTITLE IS NOT NULL')
+books = c.fetchall()
+
+connectN = sqlite3.connect(notesDB + [f for f in os.listdir(notesDB) if f.endswith('.sqlite')][0])
+cursor = connectN.cursor()
+query = '''
+			SELECT
+				ZANNOTATIONREPRESENTATIVETEXT as BroaderText,
+				ZANNOTATIONSELECTEDTEXT as SelectedText,
+				ZANNOTATIONNOTE as Note,
+				ZFUTUREPROOFING5 as Chapter,
+				ZANNOTATIONCREATIONDATE as Created,
+				ZANNOTATIONMODIFICATIONDATE as Modified,
+				ZANNOTATIONASSETID
+			FROM ZAEANNOTATION
+			WHERE ZANNOTATIONSELECTEDTEXT IS NOT NULL
+			ORDER BY ZANNOTATIONASSETID ASC,Created ASC
+			'''
+cursor.execute(query)
+notes = cursor.fetchall() 
+
+print 'Notes count: ' + str(len(notes))
+rand = random.randint(0,len(notes))
+word = notes[rand][1]
+context = notes[rand][0]
+print 'word: ' + word
+print 'meaning: ' #yet to add
+print 'context: ' + context
+book = [x for x in books if x[0]==notes[rand][-1]][0]
+print 'book: ' + book[1] + ', ' + book[2]
